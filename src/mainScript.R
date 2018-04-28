a6d5cd6ab35e72561592d8a845c0a1848578ec76
Walczakp007 <walczakp007@gmail.com>
2018-04-28 13:52:01 +0200

    Bayess

--- src/mainScript.R
+++ src/mainScript.R
@@ -1,8 +1,11 @@
+rm(list=ls())
+library(e1071)
 setwd("D:/Users/Paul/mowy")
 source("./src/nlpProcessing.R")
 source("./src/loadData.R")
 
 reviews = getData()
+
 
 split_val <- floor(0.8 * nrow(reviews))
 train_ind <- sample(seq_len(nrow(reviews)), size = split_val)
@@ -10,8 +13,19 @@ train <- reviews[train_ind, ]
 test <- reviews[-train_ind, ]
 
 
-tt = nlpProcessing(test$Text)
-df <- data.frame(text = get("content", tt))
-dataframe<-data.frame(text=unlist(sapply(tt, `[`)), stringsAsFactors=F)
 
+tt = nlpProcessing(train$Text)
+frequencies = DocumentTermMatrix(tt)
+fq <- as.data.frame(as.matrix(frequencies))
+fq$Score = unlist(train$Score)
+fq$Score = as.factor(fq$Score)
+#colnames(fq) = NULL
 
+ts = nlpProcessing(test$Text)
+frequenciest = DocumentTermMatrix(ts)
+fqt <- as.data.frame(as.matrix(frequenciest))
+fqt$Score = unlist(test$Score)
+fqt$Score = as.factor(fqt$Score)
+
+model <- naiveBayes(fq$Score ~ ., data=fq)
+predict(model, fqt)
