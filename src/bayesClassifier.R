6c50e882d67702ca1daaad2822874669545944c5
Walczakp007 <walczakp007@gmail.com>
2018-04-28 17:18:04 +0200

    Working Bayes

--- src/bayesClassifier.R
+++ src/bayesClassifier.R
@@ -2,6 +2,7 @@ library(readr)
 library(caret)
 library(tm)
 library(purrr)
+library(e1071)
 
 setwd("D:/Users/Paul/mowy")
 
@@ -15,11 +16,24 @@ divideSet <- function(data) {
 }
 
 reviews <- read_csv("./input/Reviews.csv")
-reviews <- reviews[1:25, ]
-text <- unlist(reviews$Text)
-sc <- unlist(reviews$Score)
+reviews <- reviews[1:125, ]
+
+split_val <- floor(0.8 * nrow(reviews))
+train_ind <- sample(seq_len(nrow(reviews)), size = split_val)
+train <- reviews[train_ind, ]
+test <- reviews[-train_ind, ]
+
+text <- unlist(train$Text)
+sc <- unlist(train$Score)
 sc = map(sc, divideSet)
 sc <- unlist(sc)
+
+textTestSet <- unlist(test$Text)
+scTest <- unlist(test$Score)
+scTest = map(scTest, divideSet)
+scTest <- inlist(scTest)
+
+reviewsTest = cbind(textTestSet, scTest)
 
 
 reviews = cbind(text,sc)
@@ -40,9 +54,18 @@ train <- as.data.frame(train)
 train$y <- as.factor(train$y)
 
 # Train.
-fit <- train(y ~ ., data = train, method = 'bayesglm')
-
+#fit <- train(y ~ ., data = train, method = 'bayesglm', verboseiter = TRUE)
+model <- naiveBayes(y ~ ., data=train)
+mean(fqt$Score == predict(model, fqt))
 # Check accuracy on training.
 predict(fit, newdata = train)
 
 # Test data.
+# Test data.
+data2 <- textTestSet
+corpus <- VCorpus(VectorSource(data2))
+tdm <- DocumentTermMatrix(corpus, control = list(dictionary = Terms(tdm), removePunctuation = TRUE, stopwords = TRUE, stemming = TRUE, removeNumbers = TRUE))
+test <- as.matrix(tdm)
+
+# Check accuracy on test.
+predict(fit, newdata = test)
