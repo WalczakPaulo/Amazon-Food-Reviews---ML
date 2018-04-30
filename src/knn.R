dc04fbd7c12922f252a326466b3b62f1183fb293
Walczakp007 <walczakp007@gmail.com>
2018-04-30 14:15:57 +0200

    add knn

---
+++ src/knn.R
@@ -0,0 +1,69 @@
+rm(list=ls())
+library(e1071)
+library(rpart)
+library(class) # KNN model
+library(SnowballC) 
+library(purrr)
+library(tm)
+library(readr)
+setwd("D:/Users/Paul/mowy")
+source("./src/nlpProcessing.R")
+source("./src/loadData.R")
+
+
+reviews <- read_csv("./input/Reviews.csv")
+
+
+reviews <- reviews[1:1000, ]
+reviews$Score = map(reviews$Score, divideSetNumerically)
+
+
+ss = unlist(reviews$Score)
+# Create corpus
+docs <- Corpus(VectorSource(reviews$Text))
+
+# Clean corpus
+docs <- tm_map(docs, content_transformer(tolower))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, removeWords, stopwords("english"))
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, stemDocument, language = "english")
+
+doc = nlpProcessing(reviews$Text)
+
+# Create dtm
+dtm <- DocumentTermMatrix(docs)
+
+# Transform dtm to matrix to data frame - df is easier to work with
+mat.df <- as.data.frame(data.matrix(dtm), stringsAsfactors = FALSE)
+
+# Column bind category (known classification)
+mat.df <- cbind(mat.df, ss)
+
+# Change name of new column to "category"
+colnames(mat.df)[ncol(mat.df)] <- "category"
+
+# Split data by rownumber into two equal portions
+train <- sample(nrow(mat.df), ceiling(nrow(mat.df) * .50))
+test <- (1:nrow(mat.df))[- train]
+
+# Isolate classifier
+cl <- mat.df[, "category"]
+
+# Create model data and remove "category"
+modeldata <- mat.df[,!colnames(mat.df) %in% "category"]
+
+# Create model: training set, test set, training set classifier
+knn.pred <- knn(modeldata[train, ], modeldata[test, ], cl[train])
+
+# Confusion matrix
+conf.mat <- table("Predictions" = knn.pred, Actual = cl[test])
+conf.mat
+
+# Accuracy
+(accuracy <- sum(diag(conf.mat))/length(test) * 100)
+
+# Create data frame with test data and predicted category
+df.pred <- cbind(knn.pred, modeldata[test, ])
+write.table(df.pred, file="output.csv", sep=";")
--- src/nlpProcessing.R
+++ src/nlpProcessing.R
@@ -2,14 +2,25 @@ library(tokenizers)
 library(tm)
 
 
-nlpProcessing <- function(documents) {
-  corpus = Corpus(VectorSource(tolower(documents)))
-  corpus = tm_map(corpus, removePunctuation)
-  eStopWords <- stopwords("english")
-  eStopWords <- eStopWords[eStopWords != "not"]
-  corpus = tm_map(corpus, removeWords, eStopWords)
-  corpus = tm_map(corpus, stemDocument, language="english")
-  return(corpus)
+nlpProcessing <- function(docs) {
+  docs <- Corpus(VectorSource(docs))
+  docs <- tm_map(docs, content_transformer(tolower))
+  docs <- tm_map(docs, removeNumbers)
+  docs <- tm_map(docs, removeWords, stopwords("english"))
+  docs <- tm_map(docs, removePunctuation)
+  docs <- tm_map(docs, stripWhitespace)
+  docs <- tm_map(docs, stemDocument, language = "english")
+  return(docs)
+  
+  
 }
 
 
+divideSetNumerically <- function(data) {
+  result <- ""
+  if(data < 3)
+    result <- 1
+  else 
+    result <- 5
+  return(result)
+}
